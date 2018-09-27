cc673a79b8585d32c83fb98eff08f25b6f838812
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-09-27 16:02:03 +0100

    fetch improved

--- src/components/App.js
+++ src/components/App.js
@@ -15,30 +15,34 @@ class App extends React.Component {
     this.receiveSearch = this.receiveSearch.bind(this);
   }
 
-  fetchData () {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${this.state.city}&APPID=73a0db73557b252a663d1be585b65045`)
+  fetchData(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&APPID=73a0db73557b252a663d1be585b65045`)
     .then(response => response.json())
     .then(body => {
-      this.setState({condition: body.weather[0].description}, () => {
-        fetch(`https://api.unsplash.com/search/photos?query=${this.state.condition}&client_id=ad9982e29de08ede9a9626876b58459ede10d6946a4a7dd0f66e6cd8da0b4bb4`)
-        .then(response => response.json())
-        .then(body => {
-          this.setState({
-            imagesArray: body.results,
-            currentImage: body.results[0]
-          }, () => console.log(this.state))
-        })
-        }
-      )}
-    );
+      const condition = body.weather[0].description;
+      this.setState({condition});
+      return condition;
+    })
+    .then(condition => {
+      return fetch(`https://api.unsplash.com/search/photos?query=${condition}&client_id=ad9982e29de08ede9a9626876b58459ede10d6946a4a7dd0f66e6cd8da0b4bb4`);
+    })
+    .then(response => response.json())
+    .then(body => {
+      this.setState({
+        imagesArray: body.results,
+        currentImage: body.results[0]
+      })
+    }).then(()=>console.log(this.state))
   }
 
   componentDidMount() {
-    this.fetchData ();
+    const city = this.state.city;
+    this.fetchData(city);
   }
 
   receiveSearch(search) {
-    this.setState({city: search}, () => this.fetchData ());
+    this.setState({city: search});
+    this.fetchData(search);
   }
 
   render() {
